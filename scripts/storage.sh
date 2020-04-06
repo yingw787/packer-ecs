1cc623d2773f844cd2de8a7e1e18383f3a368639
Ying Wang <yingw787.personal@gmail.com>
2020-04-06 17:08:55 -0400

    Update packer.json storage.sh fixing typos.

--- scripts/storage.sh
+++ scripts/storage.sh
@@ -7,5 +7,5 @@ set -e
 echo "### Configuring Docker Volume Storage ###"
 sudo mkdir -p /data
 sudo mkfs.ext4 -L docker /dev/xvdcy
-echo -e "LABEL=docker\t/data\t\text4\defaults,noatime\t0\t0" | sudo tee -a /etc/fstab
+echo -e "LABEL=docker\t/data\t\text4\tdefaults,noatime\t0\t0" | sudo tee -a /etc/fstab
 sudo mount -a

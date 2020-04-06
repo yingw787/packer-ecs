b4eb8e8f9f4fa3328517f4f1c78f9f6284aca61b
Ying Wang <yingw787.personal@gmail.com>
2020-04-06 16:12:03 -0400

    Add time.sh configure NTP and timezone.

---
+++ scripts/time.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -e
+
+# Configure host to use timezone
+# https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/set-time.html
+echo "### Setting timezone to $TIMEZONE ###"
+sudo tee /etc/sysconfig/clock << EOF > /dev/null
+ZONE="$TIMEZONE"
+UTC=true
+EOF
+
+sudo ln -sf /usr/share/zoneinfo/"$TIMEZONE" /etc/localtime
+
+# Use AWS NTP Sync service
+echo "server 169.254.169.123 prefer iburst" | sudo tee -a /etc/ntp.conf
+
+# Enable NTP
+sudo chkconfig ntpd on

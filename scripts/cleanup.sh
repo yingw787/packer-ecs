fb7c3ba3841ed3ec4b9c7264a4e6dd0c1613d596
Ying Wang <yingw787.personal@gmail.com>
2020-04-06 16:29:30 -0400

    Update packer.json register cleanup.sh, and add scripts/cleanup.sh clean up AMI after deploying docker and ECS on it. Not sure why a cleanup script is necessary at this point.

---
+++ scripts/cleanup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+echo "### Performing final clean-up tasks ###"
+sudo stop ecs
+sudo docker system prune -f -a
+sudo service docker stop
+sudo chkconfig docker off
+sudo rm -rf /var/log/docker /var/log/ecs/*

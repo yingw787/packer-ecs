2c4904e4e28626dae69606f735ce787b268b43b9
Ying Wang <yingw787.personal@gmail.com>
2020-04-06 16:11:54 -0400

    Add storage.sh configure ext4 partition for EBS.

---
+++ scripts/storage.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#
+# Set error flag on for all Packer shell scripts, so script exits if any command
+# fails within the script.
+set -e
+
+echo "### Configuring Docker Volume Storage ###"
+sudo mkdir -p /data
+sudo mkfs.ext4 -L docker /dev/xvdcy
+echo -e "LABEL=docker\t/data\t\text4\defaults,noatime\t0\t0" | sudo tee -a /etc/fstab
+sudo mount -a
